371a7ba78fe4a22e95ebdb4e3f02848a526f34ee
DhumanHyper <dhimanchoudhury989@gmail.com>
2024-08-30 21:51:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+list = ["lol", "arup", 'a']
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", data=list)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
