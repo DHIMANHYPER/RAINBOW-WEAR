530cf69208b4e99714c7bbc54b7786fe2491f7a4
DhumanHyper <dhimanchoudhury989@gmail.com>
2024-09-01 12:37:38 +0530

    new page added

--- app.py
+++ app.py
@@ -6,24 +6,21 @@ list = ["lol", "arup", 'a']
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html", data=list)
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+@app.route("/order")
+def order():
+    return render_template("order.html")
+@app.route("/product")
+def product():
+    return render_template("product.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-list = ["lol", "arup", 'a']
-
-
-@app.route("/")
-def hello_world():
-    return render_template("index.html", data=list)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
